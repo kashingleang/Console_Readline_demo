d4b38c46d564322e0480a6bce43e757eb2715e64
Lenovo <Lenovo@DESKTOP-BFF7370>
2022-01-22 10:17:27 +0700

    Add project files.

---
+++ Console_Readline_demo/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int num1;
+int num2;
+
+
+//get input from console
+Console.Write("Number1: ");
+string input1 = Console.ReadLine();
+Console.Write("Number2:");
+string input2 = Console.ReadLine();
+
+
+//convert to int
+num1 = Convert.ToInt32(input1);
+num2 = Convert.ToInt32(input2);
+
+//multiplication
+int result = num1 * num2;
+
+//show result
+Console.WriteLine($"{num1} X {num2} = {result}");
